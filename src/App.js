dcc7f8f7b58c10afdfba34f9a9714b93efdc3ca2
Mert Buyulu <62453255+MertBuyulu@users.noreply.github.com>
2021-11-25 21:04:06 -0600

    add search box component and make the app deploy ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import React from "react";
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { monsters: [] };
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
   }
 
   async componentDidMount() {
@@ -14,10 +18,22 @@ class App extends React.Component {
     this.setState({ monsters: data });
   }
 
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
+
   render() {
+    // destructure states from the app class
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <h1> Monster Rolodex</h1>
+        <SearchBox onSearchChange={this.onSearchChange} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
